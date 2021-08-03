aef6609906335a44455813e65360656e0d52cbd7
Elliot Metsger <emetsger@jhu.edu>
2021-08-03 10:59:19 -0400

    Properly handle 'include' and 'exclude' portions of the matrix.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import * as core from '@actions/core'
 import * as glob from '@actions/glob'
 
-const INCLUDE_KEY = 'include'
-const EXCLUDE_KEY = 'exclude'
-
 async function run(): Promise<void> {
   try {
     let dir: string = core.getInput('dir')
@@ -29,11 +26,22 @@ async function run(): Promise<void> {
 
     const key: string = core.getInput('key')
 
+    const include: boolean = core.getBooleanInput('include')
+    const exclude: boolean = core.getBooleanInput('exclude')
+
+    if (include && exclude) {
+      throw new Error(
+        '"include" and "exclude" are mutually exclusive, only one may be true'
+      )
+    }
+
     core.debug(`dir is ${dir}`)
     core.debug(`glob is ${fileGlob}`)
     core.debug(`matrix is ${matrix}`)
     core.debug(`append is ${append}`)
     core.debug(`key is ${key}`)
+    core.debug(`exclude is ${exclude}`)
+    core.debug(`include is ${include}`)
 
     const globber = await glob.create(`${dir}${fileGlob}`)
     const files = await globber.glob()
@@ -42,50 +50,44 @@ async function run(): Promise<void> {
       core.debug(`Got file: ${file}`)
     }
 
-    switch (key) {
-      case INCLUDE_KEY:
-      case EXCLUDE_KEY: {
-        core.debug(`Got include or exclude key: ${key}`)
-        matrixtype = typeof matrix
-        core.debug(`3. Typeof matrix: ${matrixtype}`)
+    if (include || exclude) {
+      core.debug(`Got include or exclude key: ${key}`)
+      matrixtype = typeof matrix
+      core.debug(`3. Typeof matrix: ${matrixtype}`)
 
-        let arr: object[] = []
-        if (key in matrix && append) {
-          // get the exising array and append to it
-          arr = matrix[key]
-        } else if (key in matrix && !append) {
-          // overwrite the existing array
-          matrix[key] = arr
-        } else {
-          // key is not in the matrix, use the empty array
-          matrix[key] = arr
-        }
-        for (const file of files) {
-          const obj = JSON.parse('{}')
-          obj[key] = file
-          arr.push(obj)
-        }
-        break
+      let arr: object[] = []
+      if (key in matrix && append) {
+        // get the exising array and append to it
+        arr = matrix[key]
+      } else if (key in matrix && !append) {
+        // overwrite the existing array
+        matrix[key] = arr
+      } else {
+        // key is not in the matrix, use the empty array
+        matrix[key] = arr
       }
-      default: {
-        core.debug(`Got key: ${key}`)
-        matrixtype = typeof matrix
-        core.debug(`4. Typeof matrix: ${matrixtype}`)
+      for (const file of files) {
+        const obj = JSON.parse('{}')
+        obj[key] = file
+        arr.push(obj)
+      }
+    } else {
+      core.debug(`Got key: ${key}`)
+      matrixtype = typeof matrix
+      core.debug(`4. Typeof matrix: ${matrixtype}`)
 
-        let arr: string[] = []
-        if (key in matrix && append) {
-          // get the exising array and append to it
-          arr = matrix[key]
-        } else if (key in matrix && !append) {
-          // overwrite the existing array
-          matrix[key] = arr
-        } else {
-          // key is not in the matrix, use the empty array
-          matrix[key] = arr
-        }
-        arr.push(...files)
-        break
+      let arr: string[] = []
+      if (key in matrix && append) {
+        // get the exising array and append to it
+        arr = matrix[key]
+      } else if (key in matrix && !append) {
+        // overwrite the existing array
+        matrix[key] = arr
+      } else {
+        // key is not in the matrix, use the empty array
+        matrix[key] = arr
       }
+      arr.push(...files)
     }
 
     core.debug(`Output matrix: ${matrix}`)
