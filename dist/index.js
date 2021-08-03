4cccc0b327372ab2611d79e63a71700b858d392e
Elliot Metsger <emetsger@jhu.edu>
2021-08-03 10:25:34 -0400

    Update dist.

--- dist/index.js
+++ dist/index.js
@@ -52,7 +52,7 @@ function run() {
             core.debug(`1. Typeof matrix: ${matrixtype}`);
             if (core.getInput('matrix').length > 0) {
                 const strMatrix = core.getInput('matrix');
-                core.debug(`parsing ${strMatrix}`);
+                core.debug(`parsing ${strMatrix} as a template`);
                 matrix = JSON.parse(`${strMatrix}`);
                 matrixtype = typeof matrix;
                 core.debug(`2. Typeof matrix: ${matrixtype}`);
