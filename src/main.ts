1774a62cc56931ca9917b529375d877fd8f1d40e
Elliot Metsger <emetsger@jhu.edu>
2021-08-03 10:25:19 -0400

    Try parsing with templates.

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function run(): Promise<void> {
 
     if (core.getInput('matrix').length > 0) {
       const strMatrix = core.getInput('matrix')
-      core.debug(`parsing ${strMatrix}`)
+      core.debug(`parsing ${strMatrix} as a template`)
       matrix = JSON.parse(`${strMatrix}`)
       matrixtype = typeof matrix
       core.debug(`2. Typeof matrix: ${matrixtype}`)
